f55f1e8b35bd8cffc1bb594db1d9aa86fce88298
ai208 <yasu6328hand@gmail.com>
2026-03-20 08:52:10 +0900

    apiの動作確認 fin

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,18 +1,27 @@
 from rest_framework import serializers
-from health.models import Weight, RouteineRecord,Routine  
+from health.models import Weight, RoutineRecord,Routine
 
-# 1 最初に、作った。
+# 1 最初に、作った。 Vaildation をしないといけない。　
 class WeightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Weight
         fields = '__all__'
+        extra_kwargs = {
+            "user": {"read_only": True}  # ← これがないと必須扱いになる　練習用
+        }
 
 class RoutineSerializer(serializers.ModelSerializer):
     class Meta:
         model = Routine
         fields = '__all__'
+        extra_kwargs = {
+            "user": {"read_only": True}  # ← これがないと必須扱いになる
+        }
 
 class RoutineRecordSerializer(serializers.ModelSerializer):
     class Meta:
-        model = RouteineRecord
+        model = RoutineRecord
         fields = '__all__'
+        extra_kwargs = {
+            "user": {"read_only": True}  # ← これがないと必須扱いになる
+        }
